b3d8d806619321af5fbed3b0012cd7697880859e
shaikh irfan <shaikhirfan08263@gamil.com>
2026-07-06 08:24:21 -0600

    updates

--- app.py
+++ app.py
@@ -29,4 +29,5 @@ def delete_employee(id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+
+    app.run(host='0.0.0.0', port=5000, debug=True)
